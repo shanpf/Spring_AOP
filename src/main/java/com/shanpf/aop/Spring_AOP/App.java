1e7be73e4f0553d651ef9ad917adf053b5d3ee06
shanpf <shanpf@yonyou.com>
2020-03-09 14:11:01 +0800

    初始化
    Signed-off-by: shanpf <shanpf@yonyou.com>

---
+++ src/main/java/com/shanpf/aop/Spring_AOP/App.java
@@ -0,0 +1,13 @@
+package com.shanpf.aop.Spring_AOP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
